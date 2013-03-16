d30aeb1a82ec75fecb4626512cb06d22f8bf0c69
vladimir-php <vladimir.php@gmail.com>
2013-03-16 13:39:36 +0200

    * Удаление лишних стилей, подчищение верстки * Удаление стилей в файле chat.css, добавление стилей контента окна * Соответствуюшие корректировки верстки в шаблоне chat.php

--- protected/components/views/chat.php
+++ protected/components/views/chat.php
@@ -1,11 +1,11 @@
 <div class="chat-window">
-	<? /*<div class="switch" onclick="Chat.switchView()"></div>
-	<div style="width:200px; background:#FF0000"></div>*/?>
-	
 	<div class="switch" onclick="Chat.switchView()"></div>
-	<div class="content"></div>
-	
-</div><br clear="all" />
+	<div class="content">
+		<div class="messages"></div>
+		<input type="text" class="message-field" name="message" /><button type="submit">send</button>
+	</div>
+	<br clear="all" />
+</div>
 <script type="text/javascript">
 	Chat.Init ();
 </script>
